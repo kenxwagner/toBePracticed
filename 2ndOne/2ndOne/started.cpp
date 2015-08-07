165b245337457da65abcd4832c2c325ba65be24d
Ken Wagner <ken_xwagner@hotmail.com>
2015-08-07 14:58:03 -0700

    Added feature for comment based on result
    comment = "This is not 'F' for -Fantastic!-";
    comment = "This is not 'D' for -Dude, good enough.-";
    comment = "This is not 'C' for -Content with this.-";
    comment = "This is almost 'B' for -Better, yes it can be!-";
    comment = "This is 'A' for -Absolutely outstanding!!!-";

--- 2ndOne/2ndOne/started.cpp
+++ 2ndOne/2ndOne/started.cpp
@@ -6,6 +6,7 @@ int main()
 {
 	int testScore;
 	char grade;
+	string comment;
 
 	//Get the numeric value
 	cout << "Enter your test score and I will\n";
@@ -14,18 +15,19 @@ int main()
 
 	//Determine the letter grade
 	if (testScore < 60)
-		grade = 'F';
+		grade = 'F', comment = "This is not 'F' for -Fantastic!-";
 	else if (testScore < 70)
-		grade = 'D';
+		grade = 'D', comment = "This is not 'D' for -Dude, good enough.-";
 	else if (testScore < 80)
-		grade = 'C';
+		grade = 'C', comment = "This is not 'C' for -Content with this.-";
 	else if (testScore < 90)
-		grade = 'B';
+		grade = 'B', comment = "This is almost 'B' for -Better, yes it can be!-";
 	else if (testScore <= 100)
-		grade = 'A';
+		grade = 'A', comment = "This is 'A' for -Absolutely outstanding!!!-";
 
 	//Display the grades
 	cout << "Your grade is " << grade << ".\n";
+	//Display extra comment
+	cout << comment << ".\n";
 	
 	return 0;
-}
